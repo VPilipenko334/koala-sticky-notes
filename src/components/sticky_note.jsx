3459bb8362964ee7254accccd2d02d3f1327b55d
Veronika <veronika.pilipenko@gmail.com>
2021-11-10 11:51:37 -0800

    adding state

--- src/components/sticky_note.jsx
+++ src/components/sticky_note.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useState } from 'react';
 
 const StickyNote = () => {
+
+    const [stickyNoteInput, setStickyNoteInput] = useState('')
 
     const addStickyNote= () => {
 
@@ -9,14 +12,20 @@ const StickyNote = () => {
     return (
         <div className="sticky-note-wrapper">
             <form onSubmit={addStickyNote} className="note-form">
-                <textarea className="note-textarea" placeholder="Add text here..."></textarea>
+                <textarea className="note-textarea" 
+                    placeholder="Add text here..."
+                    value={stickyNoteInput}
+                    onChange={e => setStickyNoteInput(e.target.value)} >
+                </textarea>
                 <button className="add-button">+</button>
             </form>
 
             <div className="sticky-note-footer">
                 11/10/11 &nbsp;
+                {stickyNoteInput}
                 <button className="delete-button">x</button>
             </div>
+          
         </div>
     )
 }
