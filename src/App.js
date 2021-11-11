5357dd62c5c76a11518d0cf6e193da825255475f
Veronika <veronika.pilipenko@gmail.com>
2021-11-10 18:16:14 -0800

    added koala icon

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-wrapper">
        <h1>Sticky Notes App</h1>
+         {/* <img src="https://media0.giphy.com/media/jxwYHbBbKiXwebfVxt/giphy.gif?cid=790b7611df025f25e025554203527b63f36e2e375db01e9d&rid=giphy.gif&ct=g" height="300" width="300"/> */}
          < AllNotes />
       </header>
     </div>
