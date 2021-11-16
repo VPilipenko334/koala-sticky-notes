0b1eac6038d82b23fdcba02fa113b96f709864b5
Veronika <veronika.pilipenko@gmail.com>
2021-11-15 21:55:15 -0800

    koala mode fully working

--- src/redux/reducers/sticky_note_reducer.js
+++ src/redux/reducers/sticky_note_reducer.js
@@ -1,40 +1,53 @@
-// import { ADD_STICKY_NOTE,
-//         REMOVE_STICKY_NOTE,
-//         REMOVE_ALL_NOTES
-// } from "../actions/sticky_note_actions"
+// import { ADD_STICKY_NOTE, REMOVE_STICKY_NOTE, REMOVE_ALL_NOTES } from './actions/sticky_note_actions'
 
-//  export const initialStickyNoteState = {
-//         lastStickyNote: null,
-//         totalStickyNotes: 0,
-//         allStickyNotes: []
+// const initialStickyNoteState = {
+//         allStickyNotes: [],
+//         totalNumberNotes: 0
 //     }
 
-
-// export const stickyNoteReducer = (oldState = {} , action) => {
+// const stickyNoteReducer = (oldState = {}, action) => {
 //     Object.freeze(oldState)
 
 //     // let newState = Object.assign({}, oldState)
 
 //     switch(action.type) {
-//         case ADD_STICKY_NOTE:
+//         case 'ADD_STICKY_NOTE': {
 //             const newState = {
-//                 lastStickyNote: new Date().toTimeString.slice(0,8), // use a library maybe?
-//                 totalStickyNotes: oldState.notes.length + 1,
-//                 allStickyNotes: [...oldState.notes, action.payload]
-//         }
-//         break;
-//         case REMOVE_STICKY_NOTE:
-//             delete newState[action.stickyNoteid]
-//             return newState;
-//         break;
-//         case REMOVE_ALL_NOTES: 
-//             return {
-//                 ...oldState,
-//                 allStickyNotes: []
+//                 allStickyNotes: [...oldState.allStickyNotes, action.payload],
+//                 totalNumberNotes: oldState.allStickyNotes.length + 1,
+//         };
+//           console.log(
+//             'After ADD_STICKY_NOTE: ', newState
+//             )
+//           return newState; 
+//     }
+        
+//         case 'REMOVE_STICKY_NOTE': {
+//             const newState = {
+//               ...oldState,
+//               allStickyNotes: oldState.allStickyNotes.filter(stickyNote => stickyNote.id !== action.payload.id),
+//               totalNumberNotes: oldState.allStickyNotes.length - 1,
+//             };
+//               console.log(
+//                 'After REMOVE_STICKY_NOTE: ', newState
+//               )
+//               return newState;
+//   }
+          
+//         case 'REMOVE_ALL_NOTES': {
+//             const newState = {
+//               ...oldState, 
+//               allStickyNotes: [],
+//               totalNumberNotes: 0
 //             }
+//             console.log(
+//               'After REMOVE_ALL_NOTES: ', newState
+//             )
+//             return newState;
+//     }
 //         default:
 //             return oldState;
-//     }
+//   }
 // }
 
-// // export default stickyNoteReducer; 
+// export default stickyNoteReducer;
